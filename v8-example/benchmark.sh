b92f9de640f71b74a1c1e573210f2bbe2ac6d828
Max Taylor <taylor.2751@osu.edu>
2023-02-20 09:51:48 -0500

    reduced benchmark noise

--- v8-example/benchmark.sh
+++ v8-example/benchmark.sh
@@ -13,8 +13,8 @@ function ensure_docker_images() {
 function start_benchmark() {
     local benchmark_page="$1"
     docker network create "$DOCKER_NETWORK_NAME" 2>/dev/null || true
-    docker container run -d --name "$SERVER_CONTAINER_NAME" --network "$DOCKER_NETWORK_NAME" v8-example
-    docker container run --network "$DOCKER_NETWORK_NAME" wrk -t4 -c100 -d30s "http://$SERVER_CONTAINER_NAME:8080/$benchmark_page"
+    docker container run -d --name "$SERVER_CONTAINER_NAME" --network "$DOCKER_NETWORK_NAME" v8-example 2>/dev/null
+    docker container run --network "$DOCKER_NETWORK_NAME" wrk -t4 -c100 -d30s "http://$SERVER_CONTAINER_NAME:8080/$benchmark_page" 2>/dev/null
 }
 
 function cleanup() {
